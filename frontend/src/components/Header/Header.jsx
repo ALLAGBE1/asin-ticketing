6a1d4444e69f70008fc87ec57cb97ade53544528
ALLAGBE1 <j.allagbe1@gmail.com>
2025-01-30 13:40:38 +0100

    [WIP] - front'

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import logo from "../../assets/logo/logo.png";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { FaTimes, FaBars } from "react-icons/fa";
 
 const Header = () => {
   const [navbar, setNavbar] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // État de connexion
+    const navigate = useNavigate();
+
+
+    useEffect(() => {
+        // Vérifie si un token est présent dans le localStorage au montage du composant
+        const token = localStorage.getItem("token");
+        setIsLoggedIn(!!token); // Met à jour l'état de connexion en fonction de la présence du token
+    }, []);
   const Navbar = [
     {
       name: "Recherche",
@@ -19,24 +28,32 @@ const Header = () => {
       link: "/profile",
     },
   ];
+
+    const handleLogout = () => {
+        // Supprime le token et les infos de localStorage
+        localStorage.removeItem("token");
+        localStorage.removeItem("userName");
+        localStorage.removeItem("userEmail");
+        localStorage.removeItem("userNumber");
+        localStorage.removeItem("isAdmin");
+
+        setIsLoggedIn(false);
+        navigate("/auth"); // Rediriger vers la page de connexion
+    };
+
   return (
     <>
-      {/* <nav className="w-full h-auto bg-gray-800 lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md"> */}
       <nav className="w-full h-auto bg-white-800  lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md">
         <div className="justify-between mx-auto lg:w-full md:items-center md:flex">
-          {/* Navbar logo & toggle button section */}
           <div>
             <div className="flex items-center justify-between py-3 md:py-5 md:block">
-              {/* Logo section */}
               <Link to="/" className="text-3xl text-orange-500 font-semibold tracking-[0.1rem]">
-                {/* Navbar */}
                 <img
                   src={logo}
                   alt="logo"
                   className="w-24 h-auto md:w-32 object-cover"
                 />
               </Link>
-              {/* Toggle button section  (we will do it later) */}
               <div className="md:hidden">
                 <button
                   className="p-2 text-gray-700 rounded-md outline-none border border-transparent focus:border-gray-400 focus:border"
@@ -58,7 +75,6 @@ const Header = () => {
             </div>
           </div>
 
-          {/* NAvbar menu items section */}
           <div
             className={`flex justify-between items-center md:block ${
               navbar ? "block" : "hidden"
@@ -75,15 +91,21 @@ const Header = () => {
                   </Link>
                 </li>
               ))}
-              {/* <button className="bg-orange-500 text-[1.1rem] font-normal text-white px-5 py-1.5 rounded lg:ml-10 md:ml-6 sm:ml-0 ml-0">
-                Register
-              </button> */}
-              <Link to="/auth" className="">
-                <button className="bg-customBlue text-[1.1rem] font-normal text-white px-5 py-1.5 rounded lg:ml-10 md:ml-6 sm:ml-0 ml-0">
-                  Connexion
-                  {/* S&apos;inscrire */}
-                </button>
-              </Link>
+                {/* Afficher "Se connecter" si non connecté, "Déconnecter" si connecté */}
+                {isLoggedIn ? (
+                    <button
+                        className="bg-customBlue text-[1.1rem] font-normal text-white px-5 py-1.5 rounded lg:ml-10 md:ml-6 sm:ml-0 ml-0"
+                        onClick={handleLogout}
+                    >
+                        Déconnecter
+                    </button>
+                ) : (
+                    <Link to="/auth" className="">
+                        <button className="bg-customBlue text-[1.1rem] font-normal text-white px-5 py-1.5 rounded lg:ml-10 md:ml-6 sm:ml-0 ml-0">
+                            Connexion
+                        </button>
+                    </Link>
+                )}
             </ul>
           </div>
         </div>
@@ -93,6 +115,105 @@ const Header = () => {
 };
 
 export default Header;
+
+
+///////////////
+
+// import logo from "../../assets/logo/logo.png";
+// import { useState } from "react";
+// import { Link } from "react-router-dom";
+// import { FaTimes, FaBars } from "react-icons/fa";
+
+// const Header = () => {
+//   const [navbar, setNavbar] = useState(false);
+//   const Navbar = [
+//     {
+//       name: "Recherche",
+//       link: "/",
+//     },
+//     {
+//       name: "Ticket",
+//       link: "/ticket",
+//     },
+//     {
+//       name: "Profil",
+//       link: "/profile",
+//     },
+//   ];
+//   return (
+//     <>
+//       {/* <nav className="w-full h-auto bg-gray-800 lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md"> */}
+//       <nav className="w-full h-auto bg-white-800  lg:px-24 md:px-16 sm:px-14 px-12 py-2 shadow-md">
+//         <div className="justify-between mx-auto lg:w-full md:items-center md:flex">
+//           {/* Navbar logo & toggle button section */}
+//           <div>
+//             <div className="flex items-center justify-between py-3 md:py-5 md:block">
+//               {/* Logo section */}
+//               <Link to="/" className="text-3xl text-orange-500 font-semibold tracking-[0.1rem]">
+//                 {/* Navbar */}
+//                 <img
+//                   src={logo}
+//                   alt="logo"
+//                   className="w-24 h-auto md:w-32 object-cover"
+//                 />
+//               </Link>
+//               {/* Toggle button section  (we will do it later) */}
+//               <div className="md:hidden">
+//                 <button
+//                   className="p-2 text-gray-700 rounded-md outline-none border border-transparent focus:border-gray-400 focus:border"
+//                   onClick={() => setNavbar(!navbar)}
+//                 >
+//                   {navbar ? (
+//                     <FaTimes
+//                       className="text-gray-400 cursor-pointer"
+//                       size={24}
+//                     />
+//                   ) : (
+//                     <FaBars
+//                       className="text-gray-400 cursor-pointer"
+//                       size={24}
+//                     />
+//                   )}
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+
+//           {/* NAvbar menu items section */}
+//           <div
+//             className={`flex justify-between items-center md:block ${
+//               navbar ? "block" : "hidden"
+//             }`}
+//           >
+//             <ul className="list-none lg:flex md:flex sm:block block gap-x-5 gap-y-16">
+//               {Navbar.map((item, index) => (
+//                 <li key={index}>
+//                   <Link
+//                     to={item.link}
+//                     className="text-gray-400 text-[1.15rem] font-medium tracking-wider hover:text-gray-200 ease-out duration-700"
+//                   >
+//                     {item.name}
+//                   </Link>
+//                 </li>
+//               ))}
+//               {/* <button className="bg-orange-500 text-[1.1rem] font-normal text-white px-5 py-1.5 rounded lg:ml-10 md:ml-6 sm:ml-0 ml-0">
+//                 Register
+//               </button> */}
+//               <Link to="/auth" className="">
+//                 <button className="bg-customBlue text-[1.1rem] font-normal text-white px-5 py-1.5 rounded lg:ml-10 md:ml-6 sm:ml-0 ml-0">
+//                   Connexion
+//                   {/* S&apos;inscrire */}
+//                 </button>
+//               </Link>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </>
+//   );
+// };
+
+// export default Header;
 
 // // import logo from "../../assets/logo/logo.png";
 // // import barreBarre from "../../assets/header/barre-menu.svg";
--- frontend/src/screens/Home/Home.jsx
+++ frontend/src/screens/Home/Home.jsx
@@ -1,76 +1,169 @@
 import { Link } from "react-router-dom";
 import ishearch from "../../assets/images-screen/image-shearch.png";
+import { useState, useEffect } from "react"; // Importez useState et useEffect
 
 function Home() {
-  return (
-    <div className="container mx-auto p-5">
-      <div className="flex flex-col sm:flex-row ">
-        <p className="text-[24px] lg:px-24 md:px-16 sm:px-14 px-12 py-2 ">
-          Bonjour, Vous cherchez un bus ? <br /> Vous êtes au bon endroit !
-        </p>
-      </div>
+  // État pour stocker le nom de l'utilisateur
+  const [userName, setUserName] = useState("");
 
-      <div className="flex flex-col sm:flex-row text-base mt-10">
-        {/* <div className="flex flex-col sm:flex-row text-base"> */}
-        <div className="w-full sm:w-1/2  flex items-center justify-center">
-          <img
-            src={ishearch}
-            alt="Mr Talon"
-            className="h-full flex justify-center items-center"
-          />
-        </div>
+    useEffect(() => {
+        // Récupérez le nom de l'utilisateur depuis le localStorage
+        const storedName = localStorage.getItem("userName");
 
-        <div className="w-full sm:w-1/2 ">
-          <div className="bg-white p-6 rounded-[34px] shadow-md sm:w-[500px]">
-            <div className="flex items-center mb-2">
-              <span className="text-xl font-semibold mr-2">📍</span>
-              <p className="text-xl font-semibold">De</p>
+        // Si le nom est trouvé, mettez à jour l'état
+        if (storedName) {
+          setUserName(storedName);
+        }
+    }, []);
+
+    return (
+        <div className="container mx-auto p-5">
+            <div className="flex flex-col sm:flex-row ">
+              {/* Afficher le nom de l'utilisateur ou une chaine de caractères par défaut si il n'y a pas de nom */}
+                <p className="text-[24px] lg:px-24 md:px-16 sm:px-14 px-12 py-2 ">
+                  Bonjour {userName || " "}, Vous cherchez un bus ? <br /> Vous êtes au bon endroit !
+                </p>
             </div>
-            <p className="text-lg mb-2">Akassato</p>
-            <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
-            <div className="flex items-center mb-2">
-              <span className="text-xl font-semibold mr-2">◎</span>
-              <p className="text-xl font-semibold">À</p>
-            </div>
-            <p className="text-lg mb-2">Saint-Michel</p>
-            <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
-            <div className="flex items-center mb-2">
-              <span className="text-xl font-semibold mr-2">👥</span>
-              <p className="text-xl font-semibold">Passagère</p>
-            </div>
-            <p className="text-lg mb-2">4 personnes</p>
-            <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
-            <div className="flex justify-between">
-              <div className="flex items-center">
-                <span className="text-xl font-semibold mr-2">📅</span>
-                <div>
-                  <p className="text-xl font-semibold">Date</p>
-                  <p className="text-lg">2025/2/1</p>
+
+            <div className="flex flex-col sm:flex-row text-base mt-10">
+                <div className="w-full sm:w-1/2  flex items-center justify-center">
+                    <img
+                        src={ishearch}
+                        alt="Mr Talon"
+                        className="h-full flex justify-center items-center"
+                    />
                 </div>
-              </div>
-              <div className="flex items-center">
-                <span className="text-xl font-semibold mr-2">🕒</span>
-                <div>
-                  <p className="text-xl font-semibold">Time</p>
-                  <p className="text-lg">7.30 AM</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
 
-      <Link to="/search" className="">
-        <button
-          type="submit"
-          className="w-[218px] bg-customBlue text-white p-4 rounded-[126px] flex justify-center mx-auto mt-10"
-          // disabled={loading}
-        >
-          Rechercher maintenant
-        </button>
-      </Link>
-    </div>
-  );
+                <div className="w-full sm:w-1/2 ">
+                    <div className="bg-white p-6 rounded-[34px] shadow-md sm:w-[500px]">
+                        <div className="flex items-center mb-2">
+                            <span className="text-xl font-semibold mr-2">📍</span>
+                            <p className="text-xl font-semibold">De</p>
+                        </div>
+                        <p className="text-lg mb-2">Akassato</p>
+                        <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
+                        <div className="flex items-center mb-2">
+                            <span className="text-xl font-semibold mr-2">◎</span>
+                            <p className="text-xl font-semibold">À</p>
+                        </div>
+                        <p className="text-lg mb-2">Saint-Michel</p>
+                        <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
+                        <div className="flex items-center mb-2">
+                            <span className="text-xl font-semibold mr-2">👥</span>
+                            <p className="text-xl font-semibold">Passagère</p>
+                        </div>
+                        <p className="text-lg mb-2">4 personnes</p>
+                        <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
+                        <div className="flex justify-between">
+                            <div className="flex items-center">
+                                <span className="text-xl font-semibold mr-2">📅</span>
+                                <div>
+                                    <p className="text-xl font-semibold">Date</p>
+                                    <p className="text-lg">2025/2/1</p>
+                                </div>
+                            </div>
+                            <div className="flex items-center">
+                                <span className="text-xl font-semibold mr-2">🕒</span>
+                                <div>
+                                    <p className="text-xl font-semibold">Time</p>
+                                    <p className="text-lg">7.30 AM</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <Link to="/search" className="">
+                <button
+                    type="submit"
+                    className="w-[218px] bg-customBlue text-white p-4 rounded-[126px] flex justify-center mx-auto mt-10"
+                >
+                    Rechercher maintenant
+                </button>
+            </Link>
+        </div>
+    );
 }
 
 export default Home;
+
+
+///////////
+
+// import { Link } from "react-router-dom";
+// import ishearch from "../../assets/images-screen/image-shearch.png";
+
+// function Home() {
+//   return (
+//     <div className="container mx-auto p-5">
+//       <div className="flex flex-col sm:flex-row ">
+//         <p className="text-[24px] lg:px-24 md:px-16 sm:px-14 px-12 py-2 ">
+//           Bonjour, Vous cherchez un bus ? <br /> Vous êtes au bon endroit !
+//         </p>
+//       </div>
+
+//       <div className="flex flex-col sm:flex-row text-base mt-10">
+//         {/* <div className="flex flex-col sm:flex-row text-base"> */}
+//         <div className="w-full sm:w-1/2  flex items-center justify-center">
+//           <img
+//             src={ishearch}
+//             alt="Mr Talon"
+//             className="h-full flex justify-center items-center"
+//           />
+//         </div>
+
+//         <div className="w-full sm:w-1/2 ">
+//           <div className="bg-white p-6 rounded-[34px] shadow-md sm:w-[500px]">
+//             <div className="flex items-center mb-2">
+//               <span className="text-xl font-semibold mr-2">📍</span>
+//               <p className="text-xl font-semibold">De</p>
+//             </div>
+//             <p className="text-lg mb-2">Akassato</p>
+//             <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
+//             <div className="flex items-center mb-2">
+//               <span className="text-xl font-semibold mr-2">◎</span>
+//               <p className="text-xl font-semibold">À</p>
+//             </div>
+//             <p className="text-lg mb-2">Saint-Michel</p>
+//             <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
+//             <div className="flex items-center mb-2">
+//               <span className="text-xl font-semibold mr-2">👥</span>
+//               <p className="text-xl font-semibold">Passagère</p>
+//             </div>
+//             <p className="text-lg mb-2">4 personnes</p>
+//             <div className="w-full border-b border-dashed border-gray-300 mb-2"></div>
+//             <div className="flex justify-between">
+//               <div className="flex items-center">
+//                 <span className="text-xl font-semibold mr-2">📅</span>
+//                 <div>
+//                   <p className="text-xl font-semibold">Date</p>
+//                   <p className="text-lg">2025/2/1</p>
+//                 </div>
+//               </div>
+//               <div className="flex items-center">
+//                 <span className="text-xl font-semibold mr-2">🕒</span>
+//                 <div>
+//                   <p className="text-xl font-semibold">Time</p>
+//                   <p className="text-lg">7.30 AM</p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <Link to="/search" className="">
+//         <button
+//           type="submit"
+//           className="w-[218px] bg-customBlue text-white p-4 rounded-[126px] flex justify-center mx-auto mt-10"
+//           // disabled={loading}
+//         >
+//           Rechercher maintenant
+//         </button>
+//       </Link>
+//     </div>
+//   );
+// }
+
+// export default Home;
